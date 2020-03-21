93a50489cd909843616cb1967818ffec202b8f58
Alvin Ng <ngalvin93@gmail.com>
2020-03-21 17:05:35 -0500

    boilerplate

--- frontend/src/utils/apolloClient.js
+++ frontend/src/utils/apolloClient.js
@@ -1,10 +1,12 @@
-import { ApolloClient } from "apollo-client";
+import { ApolloClient } from "apollo-boost"; // apollo-boost changed from apollo-client
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
-  uri: `${process.env.REACT_APP_BACKEND_URL}/graphql`
+  uri:
+    `${process.env.REACT_APP_BACKEND_URL}/graphql` ||
+    'http://localhost:1337/graphql'
 });
 const client = new ApolloClient({
   cache,
