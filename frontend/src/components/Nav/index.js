c4bef376b6dc5098bb56cf96b925d67f47f20045
Alvin Ng <ngalvin93@gmail.com>
2020-03-21 21:57:59 -0500

    slide menu

--- frontend/src/components/Nav/index.js
+++ frontend/src/components/Nav/index.js
@@ -1,12 +1,53 @@
 import React from "react";
 import Query from "../Query";
 import { Link } from "react-router-dom";
+import UIkit from 'uikit'
 
 import CATEGORIES_QUERY from "../../queries/category/categories";
 
 const Nav = () => {
+
+  const toggleMenu = () => {
+    console.log('click')
+    const menu = document.getElementById('offcanvas-menu')
+    UIkit.offcanvas(menu).show()
+  }
+
   return (
-    <div>
+    <>
+    {/* Hamburger menu button */}
+    <nav className="uk-navbar uk-navbar-container" uk-navbar>
+      <div className="uk-navbar-right">
+        <a className="uk-navbar-toggle uk-icon" uk-icon="icon: menu; ratio:4" uk-navbar-toggle-icon uk-toggle href="#offcanvas-menu" onClick={ toggleMenu }></a>
+      </div>
+    </nav>
+    {/* Slide in menu */}
+    <div id="offcanvas-menu" uk-offcanvas="overlay: true; flip: true">
+      <div class="uk-offcanvas-bar">
+          <ul class="uk-nav uk-nav-default">
+              <li class="uk-active"><a href="#">Active</a></li>
+              <li class="uk-parent">
+                  <a href="#">Parent</a>
+                  <ul class="uk-nav-sub">
+                      <li><a href="#">Sub item</a></li>
+                      <li><a href="#">Sub item</a></li>
+                  </ul>
+              </li>
+              <li class="uk-nav-header">Header</li>
+              <li><a href="#"><span class="uk-margin-small-right" uk-icon="icon: table"></span> Item</a></li>
+              <li><a href="#"><span class="uk-margin-small-right" uk-icon="icon: thumbnails"></span> Item</a></li>
+              <li class="uk-nav-divider"></li>
+              <li><a href="#"><span class="uk-margin-small-right" uk-icon="icon: trash"></span> Item</a></li>
+          </ul>
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default Nav;
+
+{/* <div>
       <Query query={CATEGORIES_QUERY} id={null}>
         {({ data: { categories } }) => {
           return (
@@ -41,8 +82,4 @@ const Nav = () => {
           );
         }}
       </Query>
-    </div>
-  );
-};
-
-export default Nav;
+    </div> */}
